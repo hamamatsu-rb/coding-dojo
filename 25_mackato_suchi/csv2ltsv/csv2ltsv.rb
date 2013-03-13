b2b60162b1cf25e65a95b13d1a177114b8c82163
uchiyama <uchiyama@elysium.co.jp>
2013-03-13 20:07:10 +0900

    first version

--- 25_mackato_suchi/csv2ltsv/csv2ltsv.rb
+++ 25_mackato_suchi/csv2ltsv/csv2ltsv.rb
@@ -2,8 +2,16 @@
 require 'csv'
 
 labels = %w{name comment status lt places newplace movies popular open date}
+header = true
 CSV.foreach("hmrb25_entry.utf8.csv") do |fields|
-  fields.each_with_index do |field, i|
-    puts "#{labels[i]}: #{field}"
+  if header 
+    header = false 
+    next
   end
+  line = []
+  fields.each_with_index do |field, i|
+    field = field.gsub(/[\t\r\n]+/, ' ')
+    line << "#{labels[i]}: #{field}"
+  end
+  puts line.join("\t")
 end
