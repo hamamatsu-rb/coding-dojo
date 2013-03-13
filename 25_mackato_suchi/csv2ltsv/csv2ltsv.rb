3fb800b5c1ce33aca2a660c185b4bf1d9d85c0d0
Masakuni Kato <kato@airs.co.jp>
2013-03-13 19:55:21 +0900

    値にラベルをつけて出力するところまでススメました

--- 25_mackato_suchi/csv2ltsv/csv2ltsv.rb
+++ 25_mackato_suchi/csv2ltsv/csv2ltsv.rb
@@ -1,4 +1,9 @@
+# coding: utf-8
 require 'csv'
-CSV.open("hmrb25_entry.sjis.csv", "r") do |row|
-  puts row.join("<>")
+
+labels = %w{name comment status lt places newplace movies popular open date}
+CSV.foreach("hmrb25_entry.utf8.csv") do |fields|
+  fields.each_with_index do |field, i|
+    puts "#{labels[i]}: #{field}"
+  end
 end
