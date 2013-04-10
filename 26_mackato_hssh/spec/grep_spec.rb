6aab1b0c81e3f85fd93f409cd537ed7a67a57039
hssh <hssh@hssh.jp>
2013-04-10 20:07:27 +0900

    Move let statements to each contexts

--- 26_mackato_hssh/spec/grep_spec.rb
+++ 26_mackato_hssh/spec/grep_spec.rb
@@ -5,17 +5,18 @@ require 'grep'
 
 describe Grep do
   describe ".grep" do
-    let(:pattern) { /456/ }
     let(:str) { "1234567890"}
     subject { Grep.grep(pattern, str) }
 
     context "pattern: /456/, str: '1234567890'" do
+      let(:pattern) { /456/ }
       it "should return 456" do
         subject.should == "456"
       end
     end
 
     context "pattern: /abc/, str: '1234567890'" do
+      let(:pattern) { /abc/ }
       it "should return nil" do
         subject.should be_nil
       end
