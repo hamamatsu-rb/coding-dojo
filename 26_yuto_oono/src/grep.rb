17f4ea329efa98ae10dc6e7f73a6bcf6e9e5f8a2
ohno <ohno@cflat-inc.com>
2013-04-10 20:29:46 +0900

    yuto_oono フォーマット対応の途中

--- 26_yuto_oono/src/grep.rb
+++ 26_yuto_oono/src/grep.rb
@@ -4,18 +4,24 @@ class Grep
   end
 
   def grep( text )
-    if @re =~ text
-      if $1 
-        return $1
-      else
-        text
-      end
-    end
+    return nil unless @re =~ text
+    return $1 if $1
+    text
   end
-
 end
 
 if __FILE__ == $0
+
+  require 'optparse'
+  require 'time'
+
+  # コマンドライン引数を解析してハッシュに格納する
+  args = {}
+  OptionParser.new do |parser|
+    parser.on('-f', '--format FORMAT') {|v| args[:format] = v}
+    parser.parse!(ARGV)
+  end
+
   exit if ARGV.empty?
   arg = ARGV[0]
   grep = Grep.new(arg)
