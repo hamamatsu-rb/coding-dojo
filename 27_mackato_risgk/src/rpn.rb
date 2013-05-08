acc49515c0fbf65d90aa46489fec3e93fecd3451
Masakuni Kato <kato@airs.co.jp>
2013-05-08 20:14:59 +0900

    implements plus operand

--- 27_mackato_risgk/src/rpn.rb
+++ 27_mackato_risgk/src/rpn.rb
@@ -1,5 +1,15 @@
 class RPN
   def calc(expression)
-    7
+    ary = expression.split()
+    stack = []    
+    ary.each do |c|
+      case c
+      when "+"
+        stack << (stack.pop + stack.pop)
+      else
+        stack << c.to_i
+      end    
+    end
+    stack[0]
   end
 end
