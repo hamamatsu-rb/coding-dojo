15a5bdee8a82a4fd313086b1794a65bb009ff7d6
Kenichi Tachibana <deadzone@be.to>
2013-05-08 20:20:13 +0900

    ２つめテストが通った

--- 27_ktachibana_yowasou/src/rpn.rb
+++ 27_ktachibana_yowasou/src/rpn.rb
@@ -14,4 +14,18 @@ class RPN
     end
     stack.pop
   end
+  def self.transform(string)
+    stack = []
+    inputs = string.split(' ')
+    while value = inputs.shift
+      case
+      when value =~ /\d+/
+        stack.push value.to_s
+      when value == '+'
+        stack.push inputs.shift
+        stack.push value
+      end
+    end
+    stack
+  end
 end
